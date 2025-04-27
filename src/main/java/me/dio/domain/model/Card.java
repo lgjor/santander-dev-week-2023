cd9d3f2d06321fdd0c30a6d5fdb6c2acd199572f
Lucas Godoy - aú desviante <lucasgodoyjor@gmail.com>
2025-04-27 12:36:29 -0300

    Adicionadas classes, feito o relacionamento para o banco de dados e criada a configuração do perfil dev por meio do arquivo application-dev.yml

---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,48 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_card")
+public class Card {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String number;
+    @Column(name= "available_limit", scale = 13, precision = 2)
+    private BigDecimal limit;
+
+    // Construtor
+    public Card(String number, BigDecimal limit) {
+        this.number = number;
+        this.limit = limit;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
---
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package me.dio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<me.dio.domain.model.User, Long> {
+}
